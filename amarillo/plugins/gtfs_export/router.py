6e9fa5df883683f25f5dbe41216cbedadeac1af5
Francia Csaba <cf@gerhardt.io>
2024-11-27 12:46:32 +0100

    Allow any api key for GRFS endpoint

--- amarillo/plugins/gtfs_export/router.py
+++ amarillo/plugins/gtfs_export/router.py
@@ -6,7 +6,7 @@ from datetime import datetime, date, timedelta
 from fastapi import APIRouter, HTTPException, Response, status, Depends
 
 from amarillo.models.Carpool import Region
-from amarillo.routers.agencyconf import verify_admin_api_key
+from amarillo.routers.agencyconf import verify_admin_api_key, verify_api_key
 from amarillo.services.regions import RegionService
 from amarillo.utils.container import container
 from fastapi.responses import FileResponse
@@ -68,7 +68,7 @@ def is_cached_1m(path : str):
                 status.HTTP_404_NOT_FOUND: {"description": "Region not found"},
         }
     )
-async def get_file(region_id: str, user: str = Depends(verify_admin_api_key)):
+async def get_file(region_id: str, user: str = Depends(verify_api_key)):
     _assert_region_exists(region_id)
     file_path = f'data/gtfs/amarillo.{region_id}.gtfs.zip'
     if is_cached_day(file_path):
@@ -76,7 +76,7 @@ async def get_file(region_id: str, user: str = Depends(verify_admin_api_key)):
         return FileResponse(file_path)
     
     # logger.info("Returning new response")
-    response = requests.get(f"{config.generator_url}/region/{region_id}/gtfs/")
+    response = requests.get(f"{config.generator_url}/region/{region_id}/gtfs")
     # cache response
     if response.status_code == 200:
         with open(file_path, "wb") as file:
@@ -93,7 +93,7 @@ async def get_file(region_id: str, user: str = Depends(verify_admin_api_key)):
                 status.HTTP_400_BAD_REQUEST: {"description": "Bad request, e.g. because format is not supported, i.e. neither protobuf nor json."}
         }
     )
-async def get_file(region_id: str, format: str = 'protobuf', user: str = Depends(verify_admin_api_key)):
+async def get_file(region_id: str, format: str = 'protobuf', user: str = Depends(verify_api_key)):
     _assert_region_exists(region_id)
     if format == 'json':
         file_path = f'data/gtfs/amarillo.{region_id}.gtfsrt.json'
